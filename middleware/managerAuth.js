e142a4af6439685e092aa7c1b4593a673aa1f178
sasya88 <126408910+sasya88@users.noreply.github.com>
2024-03-29 18:46:59 +0530

    Updated managerAuth.js

--- middleware/managerAuth.js
+++ middleware/managerAuth.js
@@ -13,9 +13,15 @@ module.exports=(req,res,next)=>{
     }
     catch(error)
     {
-        console.log(error);
+        if(error.name === 'TokenExpiredError'){
+            return res.status(400).json({
+                message:'token has expired'
+            })
+        }
+        else{
         return res.status(400).json({
             message:'Auth failed'
-        })
+        });
+    }
     }
 }
